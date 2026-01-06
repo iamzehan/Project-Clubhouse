c6e7840137f721811e76fcc3f54d1d32f9576670
Zehan <zehan.kareem@gmail.com>
2026-01-07 01:24:20 +0600

    feat(iamzehan): added sign up queries and controllers

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,11 +1,16 @@
 const db = require("../models/queries");
 const bcrypt = require("bcryptjs");
-const passport = require('../auth/passport');
+const passport = require("../auth/passport");
 
-exports.signupUser = async (req, res, next) => {
+exports.signupUserPost = async (req, res, next) => {
   try {
     const hashedPassword = await bcrypt.hash(req.body.password, 10);
-    const rows = await db.createUser(req.body.username, hashedPassword);
+    const userId = await db.createUser(req.body.username, hashedPassword);
+    const rows = await db.createProfile(
+      userId,
+      req.body.first_name,
+      req.body.last_name
+    );
     if (rows) {
       res.redirect("/login");
     } else {
@@ -17,21 +22,30 @@ exports.signupUser = async (req, res, next) => {
   }
 };
 
+exports.getAllUsers = async (req, res, next) => {
+  try {
+    const users = await db.getAllUsers();
+    req.users = users;
+    next();
+  } catch (err) {
+    next(err);
+  }
+};
 exports.postLogin = async (req, res, next) => {
   await passport.authenticate("local", (err, user, info) => {
     if (err) return next(err);
 
     if (!user) {
       return res.render("login", {
-        error: info.message
+        error: info.message,
       });
     }
-    req.logIn(user, err => {
+    req.logIn(user, (err) => {
       if (err) return next(err);
       return res.redirect("/");
     });
   })(req, res, next);
-}
+};
 
 exports.logout = async (req, res, next) => {
   await req.logout((err) => {
@@ -40,18 +54,18 @@ exports.logout = async (req, res, next) => {
     }
     res.redirect("/login");
   });
-}
+};
 
 exports.ensureAuth = (req, res, next) => {
-  if(req.isAuthenticated()){
+  if (req.isAuthenticated()) {
     return next();
   }
   res.redirect("/login");
-}
+};
 
 exports.ensureGuest = (req, res, next) => {
-  if(!req.isAuthenticated()){
+  if (!req.isAuthenticated()) {
     return next();
   }
-  res.redirect("/")
-}
+  res.redirect("/");
+};
--- src/models/queries.js
+++ src/models/queries.js
@@ -1,10 +1,21 @@
 const pool = require("./pool");
 
+exports.getAllUsers = async() => {
+  const SQL = "SELECT username from users";
+  const {rows} = await pool.query(SQL);
+  return rows;
+}
+
 exports.createUser = async (username, password) => {
   const SQL = "INSERT INTO users (username, password) VALUES ($1, $2) RETURNING *";
   const {rows} = await pool.query(SQL, [username, password]);
-  return rows;
+  return rows[0].id;
 };
+exports.createProfile = async(userId, firstname, lastname)=> {
+  const SQL = "INSERT INTO profile(first_name, last_name, user_id) VALUES ($1,$2,$3) RETURNING *";
+  const {rows} = await pool.query(SQL, [firstname, lastname, userId]);
+  return rows.length > 0;
+}
 
 exports.getUser = async (username) => {
   const SQL = `SELECT * FROM users WHERE username = $1`
