2fc65d59f2ab5c581ac6368a6075eafc325a934b
Zehan <zehan.kareem@gmail.com>
2026-01-08 00:10:13 +0600

    styles(iamzehan): added active state

--- src/views/partials/sidebar.js
+++ src/views/partials/sidebar.js
@@ -1,32 +1,43 @@
 <aside class="sidebar">
   <nav class="side-nav">
     <ul class="nav-list">
-    <li className="nav-item">
-        <a href="/" class="nav-link user">
-          <div class="profile"><i class="mi">person</i></div>
-          <span><%= currentUser.username %></span>
-        </a>
-    </li>
+
+      <!-- User profile -->
       <li class="nav-item">
+        <a href="/" class="nav-link user">
+          <div class="profile">
+            <i class="mi">person</i>
+          </div>
+          <span class="username">
+            <%= currentUser?.username || "User" %>
+          </span>
+        </a>
+      </li>
+
+      <!-- Create Message -->
+      <li class="nav-item <%= page === 'inbox' ? 'active' : '' %>">
         <a href="/messages/create" class="nav-link">
           <i class="mi">add_comment</i>
           <span>Create Message</span>
         </a>
       </li>
 
-      <li class="nav-item">
+      <!-- Clubs -->
+      <li class="nav-item <%= page === 'clubs' ? 'active' : '' %>">
         <a href="/clubs" class="nav-link">
           <i class="mi">groups</i>
           <span>Clubs</span>
         </a>
       </li>
 
+      <!-- Logout -->
       <li class="nav-item logout">
         <a href="/logout" class="nav-link">
           <i class="mi">logout</i>
           <span>Logout</span>
         </a>
       </li>
+
     </ul>
   </nav>
 </aside>
