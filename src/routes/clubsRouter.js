e9276308cead97e88e044b7cb8623f0f814db3e4
Zehan <zehan.kareem@gmail.com>
2026-01-08 03:18:16 +0600

    feat(iamzehan): created routes for club create

--- src/routes/clubsRouter.js
+++ src/routes/clubsRouter.js
@@ -1,8 +1,9 @@
 const { Router } = require("express");
 const clubsRouter = Router();
 const authController = require("../controllers/authController");
-clubsRouter.get("/", authController.ensureAuth, (req, res) => {
-  res.render("clubs");
-});
+const controller = require("../controllers/clubsController");
 
+clubsRouter.get("/", authController.ensureAuth, controller.clubGet);
+clubsRouter.get("/create", authController.ensureAuth, controller.createClubGet);
+clubsRouter.post("/create", authController.ensureAuth, controller.createClubPost);
 module.exports = clubsRouter;
