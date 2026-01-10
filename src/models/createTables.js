d2644ef6ae3aa2958594af3af49ce76fafbf13fb
Zehan <zehan.kareem@gmail.com>
2026-01-11 04:11:26 +0600

    fix(db): fixed db connection string

--- src/models/createTables.js
+++ src/models/createTables.js
@@ -1,11 +1,8 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl:
-    process.env.NODE_ENV === "production"
-      ? { rejectUnauthorized: false }
-      : false,
+  connectionString: process.env.DB_URL_PROD,
+  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
 });
 
 async function initSchema() {
